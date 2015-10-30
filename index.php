82cb91929efd43c7d6bd4ef5a53df2872c30d461
Nicholas Bentley <nickrbentley@gmail.com>
2015-10-30 14:09:07 -0400

    Added static college and degree slection in dropdowns for issue #14

--- index.php
+++ index.php
@@ -10,14 +10,25 @@
 		
 		<div class="dropdown">
 		  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-		    Dropdown
+		    Select Degree
 		    <span class="caret"></span>
 		  </button>
 		  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-		    <li><a href="#">Action</a></li>
-		    <li><a href="#">Another action</a></li>
-		    <li><a href="#">Something else here</a></li>
-		    <li><a href="#">Separated link</a></li>
+		    <li><a href="#">Associate </a></li>
+		    <li><a href="#">Bachelor</a></li>
+		    <li><a href="#">Master</a></li>
+		    <li><a href="#">Doctor</a></li>
+		  </ul>
+		</div>
+		
+		<div class="dropdown">
+		  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+		    Select College/University
+		    <span class="caret"></span>
+		  </button>
+		  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+		    <li><a href="#">Pellissippi State Community College</a></li>
+		    <li><a href="#">Roane State Community College: Clinton</a></li>
 		  </ul>
 		</div>
 		
