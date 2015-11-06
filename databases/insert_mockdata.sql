6a2074207b925c896fb724bc1db923833e52ab2f
Administrator <Administrator1@Mac2-2.local>
2015-11-06 14:19:33 -0500

    Feature #17: Creating Mockdata tables and inserting data.

---
+++ databases/insert_mockdata.sql
@@ -0,0 +1,10 @@
+INSERT INTO college(name)
+VALUES ('Pellissippi'),('University of Tennessee: Knoxville'),('Roane State'),('Yale'),('ETSU');
+
+
+INSERT INTO degree(type)
+VALUES ('Associates'),('Bachelors'),('Masters'),('Doctoral');
+
+
+INSERT INTO college_degree_cost(college_id, degree_id, cost)
+VALUES (1,1,7000),(2,2,16000),(3,1,70),(4,1,1023402),(5,2,4500);
