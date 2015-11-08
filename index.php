56842bfd536934b7201013af65bc3d64a8868420
Tanner <eeriecrate@gmail.com>
2015-11-08 18:45:29 -0500

    Added the test version of the summit button's functionality.

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 		
 		<p>Select a Degree:</p>
 		<div class="btn-group">
-			<button data-toggle="dropdown" class="btn btn-default dropdown-toggle">Associate<span class="caret"></span></button>
+			<button data-toggle="dropdown" class="btn btn-default dropdown-toggle degree">Associate<span class="caret"></span></button>
 				<ul class="dropdown-menu">
 					<li>
 						<input type="radio" id="asso_id" name="degree" value="1" checked="">
@@ -25,11 +25,11 @@
 				  		<label for="bach_id">Bachelor</label>
 				 	</li>
 					<li>
-						<input type="radio" id="mast_id" name="degree" value="2">
+						<input type="radio" id="mast_id" name="degree" value="3">
 				  		<label for="mast_id">Master</label>
 				 	</li>
 					<li>
-						<input type="radio" id="doct_id" name="degree" value="2">
+						<input type="radio" id="doct_id" name="degree" value="4">
 				  		<label for="doct_id">Doctor</label>
 				 	</li>
 				</ul>
@@ -37,7 +37,7 @@
 		
 		<p>Select a College:</p>
 		<div class="btn-group">
-			<button data-toggle="dropdown" class="btn btn-default dropdown-toggle">Pellissippi State Community College<span class="caret"></span></button>
+			<button data-toggle="dropdown" class="btn btn-default dropdown-toggle college">Pellissippi State Community College<span class="caret"></span></button>
 				<ul class="dropdown-menu">
 					<li>
 						<input type="radio" id="pell_id" name="college" value="1" checked="">
@@ -51,12 +51,21 @@
 		</div>
 		
 		<div class="btn-group" role="group" aria-label="...">
-			<button type="button" class="btn btn-default">Submit</button>
+			<button type="button" class="btn btn-default submit">Submit</button>
 		</div>
 		
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script src="lib/js/bootstrap.min.js"></script>
 		<script src="lib/js/dropdowns-enhancement.js"></script>
+		<script>
+		$( ".submit" ).click(function() {
+
+			  var degree = document.getElementsByClassName("btn btn-default dropdown-toggle degree")[0];
+			  var college = document.getElementsByClassName("btn btn-default dropdown-toggle college")[0];
+  				alert("You've chosen the degree: " + degree.firstChild.textContent + "\n at: " + college.firstChild.textContent + ".");  
+
+		});
+		</script>
 	</body>
 </html>
