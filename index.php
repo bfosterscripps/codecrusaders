692f455943099fc923fab7f2443bb15b119190d7
Tanner <eeriecrate@gmail.com>
2015-11-10 11:18:10 -0500

    Made a backup of the dropdown.

--- index.php
+++ index.php
@@ -14,39 +14,58 @@
 		
 		<p>Select a Degree:</p>
 		<div class="btn-group">
-			<button data-toggle="dropdown" class="btn btn-default dropdown-toggle degree">Associate<span class="caret"></span></button>
+			<button data-toggle="dropdown" class="btn btn-default dropdown-toggle degree">Associates<span class="caret"></span></button>
 				<ul class="dropdown-menu">
 					<li>
 						<input type="radio" id="asso_id" name="degree" value="1" checked="">
-						<label for="asso_id">Associate</label>
+						<label for="asso_id">Associates</label>
 					</li>
 					<li>
 						<input type="radio" id="bach_id" name="degree" value="2">
-				  		<label for="bach_id">Bachelor</label>
+				  		<label for="bach_id">Bachelors</label>
 				 	</li>
 					<li>
 						<input type="radio" id="mast_id" name="degree" value="3">
-				  		<label for="mast_id">Master</label>
+				  		<label for="mast_id">Masters</label>
 				 	</li>
 					<li>
 						<input type="radio" id="doct_id" name="degree" value="4">
-				  		<label for="doct_id">Doctor</label>
+				  		<label for="doct_id">Doctoral</label>
 				 	</li>
 				</ul>
 		</div>
 		
 		<p>Select a College:</p>
 		<div class="btn-group">
-			<button data-toggle="dropdown" class="btn btn-default dropdown-toggle college">Pellissippi State Community College<span class="caret"></span></button>
+			<button data-toggle="dropdown" class="btn btn-default dropdown-toggle college">Pellissippi<span class="caret"></span></button>
 				<ul class="dropdown-menu">
-					<li>
-						<input type="radio" id="pell_id" name="college" value="1" checked="">
-						<label for="pell_id">Pellissippi</label>
-					</li>
-					<li>
-						<input type="radio" id="roane_id" name="college" value="2">
-				  		<label for="roane_id">Roane State Community College: Clinton</label>
-				 	</li>
+				<?php
+				$servername = "localhost:8889";
+				$username = "root";
+				$password = "root";
+				$dbname = "college";
+
+				$conn = new mysqli($servername, $username, $password,$dbname);
+
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				} 
+				$sql = 'SELECT * from college;';
+				
+				$result = mysqli_query($conn, $sql);
+
+				if (mysqli_num_rows($result) > 0) {
+					$results = 0;
+					while($row = mysqli_fetch_assoc($result)) {
+						echo('<li>
+						<input type="radio" id = "' . $row["name"] . $row["id"] .'" name="college" value="' . $row["id"] . '" checked="">
+						<label for="' . $row["name"] . $row["id"] . '" >' . $row["name"] . '</label>
+						</li>');
+					}
+				}
+				
+				?>
+				
 				</ul>
 		</div>
 		
