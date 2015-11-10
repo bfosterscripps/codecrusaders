c0799b98ac19ce5ba114aa55c6b6b92b6f8040ac
Tanner <eeriecrate@gmail.com>
2015-11-10 10:56:59 -0500

    A working query.

--- Results.php
+++ Results.php
@@ -1,30 +1,17 @@
 <?php
-echo("PHP Established");
 $servername = "localhost:8889";
 $username = "root";
 $password = "root";
 $dbname = "college";
-echo($_GET["college"] . '</br>');
-echo($_GET["degree"] . '</br>');
-
-echo('SELECT * from college_degree_cost 
-WHERE college_id in (select id from college where name = ' . $_GET["college"] . ' ) 
-AND degree_id in (select id from degree where type = ' . $_GET["degree"] . ') </br>');
-
-
-
-
-
 
 $conn = new mysqli($servername, $username, $password,$dbname);
-
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 $sql = 'SELECT * from college_degree_cost 
-WHERE college_id in (select id from college where name = ' . $_GET["college"] . ' ) 
-AND degree_id in (select id from degree where type = ' . $_GET["degree"] . ')';
+WHERE college_id in (select id from college where name = "' . $_GET["college"] . '" ) 
+AND degree_id in (select id from degree where type = "' . $_GET["degree"] . '")';
 
 $result = mysqli_query($conn, $sql);
 
