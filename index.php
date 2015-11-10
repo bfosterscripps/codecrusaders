51ab018e5a936750e3fa8076fc5e8517f87cc796
Nicholas Bentley <nickrbentley@gmail.com>
2015-11-10 14:01:29 -0500

    Updates the index.php to actually include the nav bar and main container

--- index.php
+++ index.php
@@ -1,62 +1,78 @@
 <!DOCTYPE html>
+
 	<head>
-		<title>Wireframe: Test</title>
+		<title>Project name</title>
 		<link rel="stylesheet" href="lib/css/bootstrap.min.css">
-		<link rel="stylesheet" href="lib/css/dropdowns-enhancement.css">
+		<link href="lib/css/navbar-fixed-top.css" rel="stylesheet">
+		<meta name="author" content="Tanner McKamey, Ryan Lemons, Griff Hagen, Nicholas Bentley">
 	</head>
+	
 	<body>
-		<div class="page-header" >
-			<h1><small>Test</small></h1>
-		</div>
 
-		
+		    <!-- Fixed navbar -->
+		    <nav class="navbar navbar-default navbar-fixed-top">
+		      <div class="container">
+		        <div class="navbar-header">
+		          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		            <span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		          </button>
+		          <a class="navbar-brand" href="#">Project name</a>
+		        </div>
+		        <div id="navbar" class="navbar-collapse collapse">
+		          <ul class="nav navbar-nav">
+		            <li class="active"><a href="#">Home</a></li>
+		            <li><a href="#about">About</a></li>
+		            <li><a href="#contact">Contact</a></li>
+		          </ul>
+		        </div><!--/.nav-collapse -->
+		      </div>
+		    </nav>
 
-		
-		<p>Select a Degree:</p>
-		<div class="btn-group">
-			<button data-toggle="dropdown" class="btn btn-default dropdown-toggle">Associate<span class="caret"></span></button>
-				<ul class="dropdown-menu">
-					<li>
-						<input type="radio" id="asso_id" name="degree" value="1" checked="">
-						<label for="asso_id">Associate</label>
-					</li>
-					<li>
-						<input type="radio" id="bach_id" name="degree" value="2">
-				  		<label for="bach_id">Bachelor</label>
-				 	</li>
-					<li>
-						<input type="radio" id="mast_id" name="degree" value="2">
-				  		<label for="mast_id">Master</label>
-				 	</li>
-					<li>
-						<input type="radio" id="doct_id" name="degree" value="2">
-				  		<label for="doct_id">Doctor</label>
-				 	</li>
-				</ul>
-		</div>
-		
-		<p>Select a College:</p>
-		<div class="btn-group">
-			<button data-toggle="dropdown" class="btn btn-default dropdown-toggle">Pellissippi State Community College<span class="caret"></span></button>
-				<ul class="dropdown-menu">
-					<li>
-						<input type="radio" id="pell_id" name="college" value="1" checked="">
-						<label for="pell_id">Pellissippi State Community College</label>
-					</li>
-					<li>
-						<input type="radio" id="roane_id" name="college" value="2">
-				  		<label for="roane_id">Roane State Community College: Clinton</label>
-				 	</li>
-				</ul>
-		</div>
-		
-		<div class="btn-group" role="group" aria-label="...">
-			<button type="button" class="btn btn-default">Submit</button>
-		</div>
-		
+		    <div class="container">
+
+		      <!-- Main component for a primary marketing message or call to action -->
+		      <div class="jumbotron">
+		        <h1>Title here</h1>
+		        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus euismod sollicitudin. Pellentesque eu eleifend tortor, eu aliquam eros. Praesent luctus, lectus sed sagittis vehicula, est velit elementum diam, et tempor odio lacus viverra odio.</p>
+				
+				<div class="dropdown">
+				  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+				    Select Degree
+				    <span class="caret"></span>
+				  </button>
+				  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+				    <li><a href="#">Associate </a></li>
+				    <li><a href="#">Bachelor</a></li>
+				    <li><a href="#">Master</a></li>
+				    <li><a href="#">Doctor</a></li>
+				  </ul>
+				</div>
+				
+				<div class="dropdown">
+				  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+				    Select College/University
+				    <span class="caret"></span>
+				  </button>
+				  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+				    <li><a href="#">Pellissippi State Community College</a></li>
+				    <li><a href="#">Roane State Community College: Clinton</a></li>
+				  </ul>
+				</div>
+
+				<div class="btn-group" role="group" aria-label="...">
+				  <button type="button" class="btn btn-default">Submit</button>
+				</div>
+				
+		      </div>
+
+		    </div> <!-- /container -->
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script src="lib/js/bootstrap.min.js"></script>
-		<script src="lib/js/dropdowns-enhancement.js"></script>
+
 	</body>
+	
 </html>
